4068979bb0b5e6161294b85c1511d4add194e8da
wanhai <18825451@qq.com>
2020-05-19 15:45:08 +0800

    2020.05.19 1.Delete GNU folder.

--- GNU ARM v7.2.1 - Debug/emlib/em_assert.d
+++
@@ -1,4 +0,0 @@
-emlib/em_assert.o: ../emlib/em_assert.c \
- d:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.6\platform\emlib\inc\em_assert.h
-
-d:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.6\platform\emlib\inc\em_assert.h:
